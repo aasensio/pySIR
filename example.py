d29e9fd63a9f7b346537548f1afb9a15c3f6dc84
Andres Asensio Ramos <aasensio@iac.es>
2017-09-29 10:44:45 +0100

    Updated example

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as pl
-import sir
+import pysir
 
-sir.listLinesSIR()
+pysir.listLines()
 l = [['1',-500.,10.,1500.]]
-nLambda = sir.initializeSIR(l)
+nLambda = pysir.initialize(l)
 psf = np.loadtxt('PSF.dat', dtype=np.float32)
 setPSF(psf[:,0].flatten(), psf[:,1].flatten())
 out = np.loadtxt('model.mod', dtype=np.float32, skiprows=1)[:,0:8]
 out = np.delete(out, 2, axis=1)    
-stokes, rf = sir.synthesizeSIR(out)
+stokes, rf = pysir.synthesize(out)
 
 f, ax = pl.subplots(ncols=4, nrows=2, figsize=(16,6))
 for i in range(4):
